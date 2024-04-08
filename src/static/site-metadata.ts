d90abf69a5be29f9124950639ee40725b967229d
changchiyou <changchiyou@gmail.com>
2024-04-08 09:48:16 +0800

    config: update `site-metadata.ts`'s siteUrl, navLinks

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://running-page-changchiyous-projects.vercel.app/',
+  siteUrl: 'https://changchiyou.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -20,8 +20,8 @@ const data: ISiteMetadataResult = {
       url: 'https://github.com/changchiyou',
     },
     {
-      name: 'Reference',
-      url: 'https://github.com/yihong0618/running_page',
+      name: 'Github Repo',
+      url: 'https://github.com/changchiyou/running_page',
     },
   ],
 };
